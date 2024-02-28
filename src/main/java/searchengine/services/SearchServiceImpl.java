c6bfa09fd1393f1ec7e4ae1843f8206917da5ca6
Jonclap <iokhlopkov@gmail.com>
2024-02-28 17:38:32 +0300

    add lemmaFromQuery

--- src/main/java/searchengine/services/SearchServiceImpl.java
+++ src/main/java/searchengine/services/SearchServiceImpl.java
@@ -4,6 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import searchengine.dto.search.SearchResponse;
+import searchengine.model.Lemma;
+
+import java.util.Map;
+
 
 @RequiredArgsConstructor
 @Service
@@ -15,9 +19,10 @@ public class SearchServiceImpl implements SearchService{
     @Override
     public SearchResponse search(String query, String site, int offset, int limit) {
         SearchResponse response = new SearchResponse();
-
-
-
+        if (site.isEmpty()) {
+            site = "site_id";
+        }
+        getLemmaFromQuery(query, site);
 
         if (query.isEmpty()) {
             response.setResult(false);
@@ -27,7 +32,26 @@ public class SearchServiceImpl implements SearchService{
 
         response.setResult(true);
 
-
         return response;
+    }
+
+    public void getLemmaFromQuery(String query, String site) {
+        LemmaFinder lemmaFinder = new LemmaFinder();
+        Map<String, Integer> wordsQuery = lemmaFinder.getLemma(query);
+
+        for (Map.Entry<String, Integer> entry : wordsQuery.entrySet()) {
+            String word = entry.getKey();
+
+            Lemma lemma = siteService.getLemmaRepository().findLemma(word, siteService.getSite(site));
+
+            int frequency = lemma.getFrequency();
+            int totalPages = siteService.getPageRepository().getPageCount(siteService.getSite(site));
+
+            if ((double) frequency / totalPages > 0.9) {
+                wordsQuery.remove(word);
+            }
+
+        }
+
     }
 }
